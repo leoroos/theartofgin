cadcdc1e7d96394e3f60e0222bc1ff58f4470ee2
Leo Roos <leo.roos@deutschebahn.com>
2018-02-24 12:35:57 +0100

    add lambda and template to deploy it

---
+++ index.py
@@ -0,0 +1,14 @@
+import json
+import datetime
+
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'greeting': 'From planet mars',
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
