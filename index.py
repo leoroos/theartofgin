b1b87f1fa1d3d5241206f1cc1c58fab5d63d5eac
Leo Roos <leo.roos@deutschebahn.com>
2018-02-26 22:28:37 +0100

    add some post functions change html output

--- index.py
+++ index.py
@@ -3,12 +3,24 @@ import datetime
 
 
 def handler(event, context):
+    httpMethod = event['httpMethod']
     data = {
-        'output': 'Hello World',
+        'output': 'Http method was {}'.format(httpMethod),
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'greeting': 'From planet mars',
+        'greeting': 'From planet mars and beyond!',
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
 
+
+def post_gin_review(event, context):
+    print(json.dumps(event, indent=4))
+    print(event['body'])
+    data = {
+        'output': json.dumps(event),
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'greeting': 'Answer to gin review',
+    }
+    return {'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'}}
