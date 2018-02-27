e6399f9d7a84ae18ab64d15c1b4d768cff5303ca
Leo Roos <leo.roos@deutschebahn.com>
2018-02-27 11:10:28 +0100

    WIP add cors and reviews

--- index.py
+++ index.py
@@ -11,16 +11,53 @@ def handler(event, context):
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'headers': {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+            }}
 
 
-def post_gin_review(event, context):
-    print(json.dumps(event, indent=4))
-    print(event['body'])
-    data = {
-        'output': json.dumps(event),
-        'timestamp': datetime.datetime.utcnow().isoformat(),
-        'greeting': 'Answer to gin review',
-    }
+def reviews(event, context):
+    httpMethod = event['httpMethod']
+    print(httpMethod)
+    if httpMethod == 'GET':
+        data = {
+            'success': True,
+            'message': 'Should get a whole list of shit!',
+            'timestamp': datetime.datetime.utcnow().isoformat(),
+            'data': [{
+                'what': 'gin mare',
+                'why': 'The Great Neutralizer'
+            },{
+                'what': 'Monkey 47',
+                'why': 'Going Apeshit!!1111'
+            }
+            ]
+        }
+    elif httpMethod == 'POST':
+        data = {
+            'success': True,
+            'message': 'all good',
+            'timestamp': datetime.datetime.utcnow().isoformat(),
+        }
+    else:
+        data = { 'success': False, 'message': 'no data for you' }
+
     return {'statusCode': 200,
-            'headers': {'Content-Type': 'application/json'}}
+            'body': json.dumps(data),
+            'headers': {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+            }}
+
+
+def options(event, context):
+    response = {
+        'statusCode': 200,
+        'headers' : {
+            "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+        }
+    }
+    return response
