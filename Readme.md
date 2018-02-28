5a0916c94665869429cc97876cae613d38b5cad8
Leo Roos <leo.roos@deutschebahn.com>
2018-03-01 00:24:39 +0100

    add reviews redirect

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
-Nothing to see here, yet
+# run static webserver with
+
+    python -m http.server 1337
+
+
+# run sam local
+
+    sam local start-api --port 1331 --profile leor
--- index.py
+++ index.py
@@ -1,5 +1,13 @@
 import json
 import datetime
+from urllib import parse
+
+base_h = {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin": "*",
+}
+
+base_website = 'http://127.0.0.1:1337'
 
 
 def handler(event, context):
@@ -9,55 +17,57 @@ def handler(event, context):
         'timestamp': datetime.datetime.utcnow().isoformat(),
         'greeting': 'From planet mars and beyond!',
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*",
-            }}
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': base_h,
+    }
 
 
-def reviews(event, context):
-    httpMethod = event['httpMethod']
-    print(httpMethod)
-    if httpMethod == 'GET':
-        data = {
-            'success': True,
-            'message': 'Should get a whole list of shit!',
-            'timestamp': datetime.datetime.utcnow().isoformat(),
-            'data': [{
-                'what': 'gin mare',
-                'why': 'The Great Neutralizer'
-            },{
-                'what': 'Monkey 47',
-                'why': 'Going Apeshit!!1111'
-            }
-            ]
-        }
-    elif httpMethod == 'POST':
-        data = {
-            'success': True,
-            'message': 'all good',
-            'timestamp': datetime.datetime.utcnow().isoformat(),
-        }
-    else:
-        data = { 'success': False, 'message': 'no data for you' }
+def reviews_get(event, context):
+    data = {
+        'success': True,
+        'message': 'Should get a whole list of shit!',
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'data': [{
+            'what': 'gin mare',
+            'why': 'The Great Neutralizer'
+        }, {
+            'what': 'Monkey 47',
+            'why': 'Going Apeshit!!1111'
+        }]
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': base_h
+    }
 
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*",
-            }}
+
+def reviews_post(event, context):
+    params = parse.parse_qs(event['body'])
+    ginReview = params['ginReview'][0]
+    print('Gin review:' , ginReview)
+    data = {
+        'success': True,
+        'message': 'Got the body:' + json.dumps(event['body']),
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+    }
+    h = base_h.copy()
+    h.update({ 'Location': base_website + '/reviews.html', })
+    return {
+        'statusCode': 302,
+        'body': json.dumps(data),
+        'headers': h
+    }
 
 
 def options(event, context):
-    response = {
+    return {
         'statusCode': 200,
-        'headers' : {
+        'headers': {
             "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
             "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
             "Access-Control-Allow-Origin": "*",
         }
     }
-    return response
